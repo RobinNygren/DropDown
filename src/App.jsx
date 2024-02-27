73db3d3b2c0bf3b8fdec48feedb752b17f7e6dbb
Robin <robin_nygren@outlook.com>
2024-02-27 21:56:58 +0100

    dropdown working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DropdownBox from "./components/DropdownBox/DropdownBox";
-import Dropdown from "./components/DropdownBox/Dropdown/Dropdown";
+
 
 function App() {
 
--- src/components/DropdownBox/DropdownBox.jsx
+++ src/components/DropdownBox/DropdownBox.jsx
@@ -1,19 +1,38 @@
-import Dropdown from "./Dropdown/Dropdown";
 import dropdown from "./dropdown.json";
+import { useState } from 'react'
 import "./DropdownBox.css";
 
+
 const DropdownBox = () => {
-    const data = dropdown.data
+    const data = dropdown.data;
+    const [selectedPerson, setSelectedPerson] = useState(null);
+
+    const handleSelect = (event) => {
+        const selected = data.find(person => `${person.firstname} ${person.lastname}` === event.target.value);
+        setSelectedPerson(selected);
+        console.log(selected)
+    };
+
     return (
         <>
-        <h2 className="info">Information</h2>
-        <select className="DropdownBox">
-        {data.map((info)=>{
-            return (
-                <Dropdown firstname={info.firstname} lastname={info.lastname}/>
-                )
-        })}
-        </select>
+            <h2 className="info">Information</h2>
+            <select className="DropdownBox" onChange={handleSelect}>
+                <option>Välj en person</option>
+                {data.map((info, index) => (
+                    <option key={index}>
+                        {info.firstname} {info.lastname}
+                    </option>
+                ))}
+            </select>
+            {selectedPerson && (
+                <div className="UserInfo">
+                    <p>Byxfärg: {selectedPerson.pant_color}</p>
+                    <p>Favoritmat: {selectedPerson.favorite_food}</p>
+                    <p>Favoritverktyg: {selectedPerson.favorite_tool}</p>
+                    <p>Husdjur: {selectedPerson.pet}</p>
+                    <p>Hobby: {selectedPerson.hobby}</p>
+                </div>
+            )}
         </>
     );
 }
